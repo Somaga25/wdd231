8a51996d709ea7b482535383bb2f2e7966d7186a
Somaga25 <ceke2@byupathway.edu>
2026-05-16 23:50:50 +0100

    relocation of files

--- scripts/date.js
+++
@@ -1,6 +0,0 @@
-const currentYear = document.querySelector('#currentyear');
-const lastModified = document.querySelector('#lastModified');
-
-currentYear.textContent = new Date().getFullYear();
-
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
